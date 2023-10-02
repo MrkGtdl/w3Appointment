3f235b0ee408c0a680a02ccc87cbe3e582f65f0c
MrkGtdl <markaustriagatdula@gmail.com>
2023-10-02 23:46:08 +0800

    initial commit

---
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class LoginRegisterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest')->except([
+            'logout', 'dashboard'
+        ]);
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            // 'fname' => 'required|string|max:250',
+            // 'lname' => 'required|string|max:250',
+            // 'email' => 'required|email|max:250|unique:users',
+            'mobile_phone' => 'required|max:250|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        User::create([
+            // 'name' => $request->name,
+            // 'email' => $request->email,
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'mobile_phone' => $request->mobile_phone,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $credentials = $request->only('mobile_phone', 'password');
+        Auth::attempt($credentials);
+        $request->session()->regenerate();
+
+        return redirect()->route('dashboard')
+        ->withSuccess('You have successfully registered & logged in!');
+    }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'mobile_phone' => 'required',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials))
+        {
+            $request->session()->regenerate();
+            return redirect()->route('dashboard')
+                ->withSuccess('You have successfully logged in!');
+        }
+
+        return back()->withErrors([
+            'mobile_phone' => 'Your provided credentials do not match in our records.',
+        ])->onlyInput('mobile_phone');
+
+    } 
+
+    public function dashboard()
+    {
+        if(Auth::check())
+        {
+            return view('auth.dashboard');
+        }
+        
+        return redirect()->route('login')
+            ->withErrors([
+            'mobile_phone' => 'Please login to access the dashboard.',
+        ])->onlyInput('mobile_phone');
+    } 
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')
+            ->withSuccess('You have logged out successfully!');;
+    }  
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,87 @@
+@extends('auth.layouts')
+
+@section('content')
+
+<div class="row justify-content-center mt-5">
+    <div class="col-md-8">
+
+        <div class="card">
+            <div class="card-header">Register</div>
+            <div class="card-body">
+                <form action="{{ route('store') }}" method="post">
+                    @csrf
+                    <!-- <div class="mb-3 row">
+                        <label for="name" class="col-md-4 col-form-label text-md-end text-start">Name</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}">
+                            @if ($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
+                        <div class="col-md-6">
+                          <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+                            @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
+                        </div>
+                    </div> -->
+                    
+                    <div class="mb-3 row">
+                        <label for="fname" class="col-md-4 col-form-label text-md-end text-start">First Name</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control @error('fname') is-invalid @enderror" id="fname" name="fname" value="{{ old('fname') }}">
+                            @if ($errors->has('fname'))
+                                <span class="text-danger">{{ $errors->first('fname') }}</span>
+                            @endif
+                        </div>
+                    </div>                    <div class="mb-3 row">
+                        <label for="lname" class="col-md-4 col-form-label text-md-end text-start">Last Name</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control @error('lname') is-invalid @enderror" id="lname" name="lname" value="{{ old('lname') }}">
+                            @if ($errors->has('lname'))
+                                <span class="text-danger">{{ $errors->first('lname') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="mobile_phone" class="col-md-4 col-form-label text-md-end text-start">Mobile Number</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control @error('mobile_phone') is-invalid @enderror" id="mobile_phone" name="mobile_phone" value="{{ old('mobile_phone') }}">
+                            @if ($errors->has('mobile_phone'))
+                                <span class="text-danger">{{ $errors->first('mobile_phone') }}</span>
+                            @endif
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="mb-3 row">
+                        <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
+                        <div class="col-md-6">
+                          <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                            @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="password_confirmation" class="col-md-4 col-form-label text-md-end text-start">Confirm Password</label>
+                        <div class="col-md-6">
+                          <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Register">
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    </div>    
+</div>
+    
+@endsection
